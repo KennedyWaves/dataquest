fda4e1c1e9488d2eb34f0f57a97f91b1c4904e37
Igor Aviz <igorleonardodeaviz@gmail.com>
2019-12-21 00:40:56 -0300

    Trava no check box

--- lib/question.dart
+++ lib/question.dart
@@ -9,9 +9,9 @@ class _question_state extends State<question> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            appBar: AppBar(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
           title: Text('Formulário'),
           bottom: TabBar(
             tabs: <Widget>[
@@ -26,6 +26,152 @@ class _question_state extends State<question> {
               ),
             ],
           ),
-        )));
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Eixo1(),
+            Eixo2(),
+            Eixo3(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Eixo1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            height: 75,
+            child: Center(
+              child: new Text(
+                "EIXO: SUSTENTABILIDADES E BOM VIVER",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            height: 75,
+            margin: EdgeInsets.all(2.0),
+            padding: EdgeInsets.all(6),
+            child: Center(
+              child: new Text(
+                "Marque as questões/indicadores considerando a escala no qual 0 é nunca e 5 é sempre",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Container(
+              height: 50,
+              margin: EdgeInsets.only(right: 10, left: 10, top: 10, bottom: 2),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.green,
+              ),
+              child: Center(
+                child: Text(
+                  '1 - QUALIDADE DA MORADIA',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              )),
+          Container(
+            margin: EdgeInsets.only(right: 10, left: 10, top: 2, bottom: 5),
+            decoration: BoxDecoration(
+              color: Colors.green[50],
+            ),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              // FIRT row (single item)
+              Padding(
+                padding: EdgeInsets.only(bottom: 15, top: 15),
+                child: Text(
+                  '1.1) A estrutura de sua casa é segura?',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+
+              // SECOND row
+              Padding(
+                padding: EdgeInsets.only(bottom: 15, top: 0),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Eixo2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.green,
+            height: 50,
+            child: Center(
+              child: new Text(
+                "EIXO: RAZÕES HUMANAS",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Eixo3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.purple,
+            height: 50,
+            child: Center(
+              child: new Text(
+                "EIXO: CONSEQUENCIAS AMBIENTAIS",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
